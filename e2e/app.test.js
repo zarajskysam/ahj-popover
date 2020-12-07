316fdb0f7aec38d0b1f2e496d11a3961651cd60a
Semyon Zarajsky <zarajskysam92@gmail.com>
2020-12-07 22:18:07 +0300

    commit added 2

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-undef */
 import puppetteer from 'puppeteer';
 
-jest.setTimeout(30000); // default puppeteer timeout
+jest.setTimeout(30000);
 describe('popover', () => {
   let browser = null;
   let page = null;
   const baseUrl = 'http://localhost:9000';
   beforeAll(async () => {
     browser = await puppetteer.launch({
-    //   headless: false, // show gui
+    //   headless: false, 
     //   slowMo: 100,
-    //   devtools: true, // show devTools
+    //   devtools: true, 
     });
     page = await browser.newPage();
   });
